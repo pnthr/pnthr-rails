6bd610976e095689e37f2a6b9b32af89c4775053
Clay McIlrath <clay.mcilrath@gmail.com>
2014-03-26 14:29:02 -0400

    adding in the go file

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -3,56 +3,56 @@ class InstancesController < ApplicationController
 
   # GET /applications
   def index
-    @applications = Instance.all
+    @instances = Instance.all
   end
 
-  # GET /applications/1
+  # GET /instances/1
   def show
   end
 
-  # GET /applications/new
+  # GET /instances/new
   def new
-    @application = Instance.new
+    @instance = Instance.new
   end
 
-  # GET /applications/1/edit
+  # GET /instances/1/edit
   def edit
   end
 
-  # POST /applications
+  # POST /instances
   def create
-    @application = Instance.new(instance_params)
+    @instance = Instance.new(instance_params)
 
-    @application.identifier = SecureRandom.urlsafe_base64(nil, false)
-    @application.secret = SecureRandom.urlsafe_base64(nil, false)
-    @application.password = Digest::MD5.hexdigest(instance_params[:password])
+    @instance.identifier = Digest::MD5.hexdigest(nil, false)
+    @instance.secret = Digest::MD5.hexdigest(nil, false)
+    @instance.password = Digest::MD5.hexdigest(instance_params[:password])
 
-    if @application.save
-      redirect_to @application, notice: 'Instance was successfully created.'
+    if @instance.save
+      redirect_to @instance, notice: 'Instance was successfully created.'
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /applications/1
+  # PATCH/PUT /instances/1
   def update
-    if @application.update(instance_params)
-      redirect_to @application, notice: 'Instance was successfully updated.'
+    if @instance.update(instance_params)
+      redirect_to @instance, notice: 'Instance was successfully updated.'
     else
       render action: 'edit'
     end
   end
 
-  # DELETE /applications/1
+  # DELETE /instances/1
   def destroy
-    @application.destroy
-    redirect_to applications_url, notice: 'Instance was successfully destroyed.'
+    @instance.destroy
+    redirect_to instances_url, notice: 'Instance was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_instance
-      @application = Instance.find(params[:id])
+      @instance = Instance.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
